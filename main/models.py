0bcfbe2050e464b4db6e689b85698727fe550461
bbedwin <bbedwin@bharathbrands.in>
2023-05-18 12:03:21 +0300

    feat: add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Sightings(models.Model):
+    sighting_id = models.AutoField(primary_key=True)
+    sighting_datetime = models.DateTimeField(auto_now_add=True)
+    species = models.CharField(max_length=255, null=True, blank=False)
+
+    class Meta:
+        db_table = "wildlife_sightings"
+        ordering = ["-sighting_datetime"]
+
+    def __str__(self) -> str:
+        return self.sighting_id
