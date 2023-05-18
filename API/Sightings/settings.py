73568c4c4c8781384d2ef439e95eecb19b4046d7
bbedwin <bbedwin@bharathbrands.in>
2023-05-18 19:47:31 +0300

    feat: django-cors allowed urls

--- API/Sightings/settings.py
+++ API/Sightings/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 # Password validation
